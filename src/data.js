ac5fbf646d060729b44572009068f731132dc208
Prapti Bargir <praptibargir21@gmail.com>
2024-06-30 19:35:42 +0530

    Data added

---
+++ src/data.js
@@ -0,0 +1,45 @@
+const blogs = [
+    {
+        title: "Lifesaving “Git” Commands: Mastering Emergency Situations",
+        content: "Git, the widely used version control system, is a powerhouse tool for developers. It allows seamless tracking of changes, collaborative work on projects, and maintaining a complete history of all edits and updates. However, sometimes things can go wrong — a wrong branch merge, accidental commit deletions, or other mishaps. Knowing some advanced Git commands can save the day. Here’s a guide to some of the “secret” lifesaving Git commands that every developer should know.",
+        author:{
+            name:"Mertcan Arguç",
+            auth:""
+        },
+        date:"May 5, 2024",
+        category:["Git","Github"]
+    },
+    {
+        title: "How to Get Users to Your App (And Not Waste Time)",
+        content: "Recently, I built an app in 6 hours that makes $1,500/mo. Sounds like an epic side hustle, right? But here’s the catch. It took 1,000+ hours to market the app to get users to it. In this guide, I’ll show you how to build an app that actually gets users. My Biggest Mistake When I coded my first apps as a beginner, I did it like this: Get a unique idea. Code the app. Publish it.Wait for users to come. Well… they never came. Later I learned that everything was wrong with this approach. Literally everything.",
+        author:{
+            name:"Artturi Jalli",
+            auth:""
+        },
+        date:"Apr 18, 2024",
+        category:["Engagement"]
+    },
+    {
+        title: "15 Time-Saving Websites Every Developer Needs",
+        content: "Ever thought that there aren’t enough hours in the day for all your never-ending development tasks? You are not alone.From creating pixel-perfect designs to managing advanced back-end logic, web development comes with time-consuming challenges.Worry not, fellow developers! This post offers a treasure mine of powerful websites that help improve your workflow and save you valuable time for what matters: creating excellent stuff.",
+        author:{
+            name:"Rehan Pinjari",
+            auth:""
+        },
+        date:"Apr 29, 2024",
+        category:["Javascript"]
+    },
+    {
+        title: "You don’t actually NEED if statements (ever)",
+        content: "Sure they’re a nice and easy way to create control flow, but you can write many billions of lines of conditional JS code without a SINGLE if statement. And there are many situations where a different construct shows what you wanna do way more clearly — something we can’t ignore as long we write code for humans. Not to mention lower verbosity and shorter code…",
+        author:{
+            name:"Tari Ibaba",
+            auth:""
+        },
+        date:"Apr 22, 2024",
+        category:["Javascript"]
+    }
+
+]
+
+export default blogs
